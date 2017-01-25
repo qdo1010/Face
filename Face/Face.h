21dc703b3ae308d3191f1679cdde5c7e339f0920
qhdo1010 <do.q@husky.neu.edu>
2017-01-25 15:57:19 -0500

    Initial Commit

---
+++ Face/Face.h
@@ -0,0 +1,19 @@
+//
+//  Face.h
+//  Face
+//
+//  Created by HongQuan Do on 1/25/17.
+//  Copyright © 2017 HongQuan Do. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Face.
+FOUNDATION_EXPORT double FaceVersionNumber;
+
+//! Project version string for Face.
+FOUNDATION_EXPORT const unsigned char FaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Face/PublicHeader.h>
+
+
